c01b2f0ecd628fe2ad24de155ee72195d066f964
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-09-22 15:17:31 +0200

    agregamos boton para limpiar la busqueda

--- src/Routes/Buscador.jsx
+++ src/Routes/Buscador.jsx
@@ -14,7 +14,7 @@ export const Buscador = () => {
 
   const onSubmitForm = (e) => {
     e.preventDefault()
-    if (busqueda.length === 0) return
+    if (busqueda.trim().length === 0) return
     console.log(busqueda)
     fetchPeliculas()
     setBusqueda("")
@@ -33,49 +33,56 @@ export const Buscador = () => {
 
 
   return (
-    <div className='container mb-3'>
-      <form className='container-buscador d-flex' onSubmit={onSubmitForm}>
-        <div>
-          <input
-            type="text"
-            className="form-control"
-            name="busqueda"
-            placeholder='Introduce Pelicula'
-            value={busqueda}
-            onChange={handleInput}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+    <div className="container mb-5">
+  
+      <div className="d-flex justify-content-center mb-4">
+        <form className="w-100 w-md-50" onSubmit={onSubmitForm}>
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              name="busqueda"
+              placeholder="Introduce Pelicula"
+              value={busqueda}
+              onChange={handleInput}
+            />
+            <button type="submit" className="btn btn-primary">Buscar</button>
+            <button type="submit" className="btn btn-primary">Limpiar</button>
+          </div>
+        </form>
+      </div>
 
-      <div className="container mt-4">
-        <div className="row g-3">
-          {peliculas.map(item => (
-            <div className="col-6 col-md-4 col-lg-3" key={item.id}>
-              <div className="card h-100">
-                <img
-                  src={`${urlImage}${item.poster_path}`}
-                  className="card-img-top"
-                  alt="pelicula-imagen"
-                  style={{ height: "200px", objectFit: "cover" }}
-                />
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{item.original_title}</h5>
-                  <p className="card-text flex-grow-1 overflow-hidden"
-                    style={{
-                      display: "-webkit-box",
-                      WebkitLineClamp: 3,
-                      WebkitBoxOrient: "vertical",
-                      textOverflow: "ellipsis"
-                    }}> {item.overview}
-                    </p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
+ 
+      <div className="row g-3">
+        {peliculas.map(item => (
+          <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+            <div className="card h-100 d-flex flex-column">
+              <img
+                src={`${urlImage}${item.poster_path}`}
+                className="card-img-top"
+                alt={item.original_title}
+                style={{ height: "200px", objectFit: "cover" }}
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title text-truncate" title={item.original_title}>
+                  {item.original_title}
+                </h5>
+                <p className="card-text flex-grow-1 overflow-hidden"
+                  style={{
+                    display: "-webkit-box",
+                    WebkitLineClamp: 3,
+                    WebkitBoxOrient: "vertical",
+                    textOverflow: "ellipsis"
+                  }}>
+                  {item.overview}
+                </p>
+                <a href="#" className="btn btn-primary mt-auto">Ver detalle</a>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </div >
+    </div>
+    
   )
 }
