0a65c5d7a652b5d6dc47d1adb07aeac521bad9b8
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-09-22 12:07:43 +0200

    cambiamos fuente y terminamos seccion de logo

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 export const Logo = () => {
   return (
     <div >
-      <div className='container-logo'>
-        <img src='src/assets/fondo-cine.png' alt='logo' width={"240"} height={"240"}/>
-        <h1>MovieSearch</h1>
+      <div className='container-logo d-flex align-items-center justify-content-start px-5'>
+        <img src='src/assets/fondo-cine.png' alt='logo' width={"180"} height={"150"} />
+        <div className="ms-4 d-flex flex-column">
+          <h1>Movie<span>Search</span></h1>
+          <p className="mb-0">Pasión por el cine</p>
+        </div>
       </div>
     </div>
   )
--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
   return (
     <div className='container'>
-        
+        <p>About</p>
     </div>
   )
 }
