9f4e0358a7a0a76a1d9979256e47425f7a7b095e
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-09-22 13:10:31 +0200

    Creamos y controlamos un input para poder buscar las peliculas

---
+++ src/Components/FetchPeliculas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FetchPeliculas = () => {
+  return (
+    <div>FetchPeliculas</div>
+  )
+}
--- src/Routes/Buscador.jsx
+++ src/Routes/Buscador.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
+
 
 export const Buscador = () => {
+  const [busqueda, setBusqueda] = useState('');
+
+  const handleInput = ({ target }) => {
+    setBusqueda(target.value)
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    if (busqueda.length === 0) return
+    console.log(busqueda)
+    setBusqueda("")
+  };
+  
   return (
-    <div className='container'>Buscador</div>
+    <div className='container mb-3'>
+      <form className='container-buscador d-flex' onSubmit={onSubmitForm}>
+        <div>
+          <input
+            type="text"
+            className="form-control"
+            name="busqueda"
+            placeholder='Introduce Pelicula'
+            value={busqueda}
+            onChange={handleInput}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
   )
 }
