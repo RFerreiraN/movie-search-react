7c60e9ef2fb3f0a46edcfc453ece14c4175529ee
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-09-25 22:15:06 +0200

    mejoramos estilos de la tajeta de peliculas en componente detalles, ahora daremos contexto para poder mantener el estado a la hora de volver del componente detalle a la busqueda con el boton volver

--- src/Routes/Detalles.jsx
+++ src/Routes/Detalles.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { href, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 let apiKey = '755ce7a7d4fe1b26cff21b7995d49852';
 let urlApi = 'https://api.themoviedb.org/3/movie';
@@ -10,12 +10,17 @@ export const Detalles = () => {
 
   const { id } = useParams();
   const [pelicula, setPelicula] = useState(null)
+  const navigate = useNavigate();
 
   const fetchDetalle = async () => {
     const response = await fetch(`${urlApi}/${id}?api_key=${apiKey}`);
     const data = await response.json()
     setPelicula(data)
     console.log(data)
+  }
+
+  const volverBusqueda = ()=> {
+      navigate(-1)
   }
 
   useEffect(() => {
@@ -25,17 +30,18 @@ export const Detalles = () => {
   if (!pelicula) return <p>Cargando...</p>
 
   return (
-    <div className='container d flex mt-5'>
-      <div className="card" style={{width: "300px", height: "auto"}}>
-        <img src={`${urlImage}${pelicula.poster_path}`} className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <p className="card-title" style={{fontSize : "18px"}}><span>{pelicula.overview}</span></p>
-            <p className="card-text" style={{fontSize : "13px"}}><span>Fecha de estreno : {pelicula.release_date}</span></p>
-            <p className="card-text" style={{fontSize : "13px"}}><span>Puntuación : {pelicula.vote_average}/10</span></p>
-            <a href="#" className="link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mt-auto" style={{fontSize : "15px"}}>Añadir a Favoritas</a>
-            <a href="#" className="link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mt-auto" style={{fontSize : "15px"}}>Volver</a>
+
+     <div className="container-detail">
+          <div className='card-detail'>
+            <img src={`${urlImage}${pelicula.poster_path}`} alt="..." />
+            <p className="card-title"><span>{pelicula.overview}</span></p>
+          <p className="card-text" ><span>Fecha de estreno : {pelicula.release_date}</span></p>
+          <p className="card-text" ><span>Puntuación : {pelicula.vote_average}/10</span></p>
+            <div className="d-flex justify-content-center gap-3 mt-3">
+             <a href="#" className="link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mt-auto" style={{ fontSize: "15px" }}>Añadir a Favoritas</a>
+             <a href="#" className="link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover mt-auto" style={{ fontSize: "15px" }} onClick={volverBusqueda}>Volver</a>
+           </div>
           </div>
-      </div>
-    </div>
+     </div>
   )
 }
